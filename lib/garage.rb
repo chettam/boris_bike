9c606ec6e1fd6aa774a2590f23e9a6c8b4165d1a
Jean-Baptiste Blanc <jbblanc@gmail.com>
2013-11-19 09:33:49 +0000

    garage dock broken bikes

--- lib/garage.rb
+++ lib/garage.rb
@@ -5,6 +5,10 @@ class Garage
 	# include all the methods we need in this class (from the module)
 	include BikeContainer
 
+	def initialize
+
+	end
+
 	def dock(bike)
 		store(bike)
 	end
--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -1,17 +1,24 @@
 require_relative '../lib/garage'
-
 describe Garage  do
 
 		let (:garage) {Garage.new} 
-		let (:bike) {Bike.new}
+		let (:broken_bike) {Bike.new}
 		let (:van) {Van.new}
 
 	context "Should" do
 
-		it " dock a bike" do
-			garage.dock(bike)
+		it "dock a bike" do
+			broken_bike.break
+			garage.dock(broken_bike)
+			expect(garage.bikes.first).to eq(broken_bike)
 		end
 
+		it "dock only broken bike" do
+			broken_bike.break
+			garage.dock(broken_bike)
+			bike = garage.bikes.first
+			expect(bike).to be_broken
+		end
 	end
 end
 
